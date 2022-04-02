4133b41916db34593def3da8859b1b4c5edbcb57
AB SIDDIQUE <31104573+SiddiqueAb@users.noreply.github.com>
2022-04-02 13:13:57 +0600

    new site

---
+++ asset/js/main.js
@@ -0,0 +1,19 @@
+/*drag the button*/
+$(document).ready(function() {
+	$('.btn').draggable();
+	$('.modal').draggable();
+
+});
+
+/*work with wow js*/
+new WOW().init();
+
+/*$(document).ready(function(){
+$('.carousel').carousel({
+carouselWidth:930,
+carouselHeight:330,
+directionNav:true,
+shadow:true,
+buttonNav:'bullets'
+});
+});*/
